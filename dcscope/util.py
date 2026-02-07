de69ac021dcb80e983dae77203c0f673f975b390
Paul Müller <dev@craban.de>
2026-02-08 00:24:51 +0100

    ref: cleanup

--- dcscope/util.py
+++ dcscope/util.py
@@ -6,22 +6,6 @@ import os.path
 from dclab.util import hashobj  # noqa: F401
 
 
-def strip_common_prefix_suffix(string_list: list[str]) -> list[str]:
-    sl = string_list
-
-    # cut common prefixes
-    prefix = os.path.commonprefix(sl)
-    sl = [n[len(prefix):] for n in sl]
-
-    # cut common suffixes
-    sl = [n[::-1] for n in sl]  # reverse order
-    suffix = os.path.commonprefix(sl)
-    sl = [n[len(suffix):] for n in sl]
-
-    sl = [n[::-1] for n in sl]  # reverse order
-    return sl
-
-
 def get_valid_filename(value):
     """
     Return the given string converted to a string that can be used
@@ -50,3 +34,19 @@ def get_valid_filename(value):
 
     ret = ret.strip(".")
     return ret
+
+
+def strip_common_prefix_suffix(string_list: list[str]) -> list[str]:
+    sl = string_list
+
+    # cut common prefixes
+    prefix = os.path.commonprefix(sl)
+    sl = [n[len(prefix):] for n in sl]
+
+    # cut common suffixes
+    sl = [n[::-1] for n in sl]  # reverse order
+    suffix = os.path.commonprefix(sl)
+    sl = [n[len(suffix):] for n in sl]
+
+    sl = [n[::-1] for n in sl]  # reverse order
+    return sl
