1eb93021ab228816ec9a27b431ac77f29e0f1e48
Paul Müller <dev@craban.de>
2026-05-06 13:33:55 +0200

    enh: enable rubberband-resize for all subwindows

--- dcscope/gui/widgets/mdi_subwindow_wo_close.py
+++ dcscope/gui/widgets/mdi_subwindow_wo_close.py
@@ -5,8 +5,11 @@ class MDISubWindowWOButtons(QtWidgets.QMdiSubWindow):
     def __init__(self, *args, **kwargs):
         super(MDISubWindowWOButtons, self).__init__(*args, **kwargs)
         self.setSystemMenu(None)
-        self.setWindowFlags(QtCore.Qt.WindowType.CustomizeWindowHint
-                            | QtCore.Qt.WindowType.WindowTitleHint)
+        self.setOption(
+            QtWidgets.QMdiSubWindow.SubWindowOption.RubberBandResize)
+        self.setWindowFlags(
+            QtCore.Qt.WindowType.CustomizeWindowHint
+            | QtCore.Qt.WindowType.WindowTitleHint)
 
     def closeEvent(self, closeEvent):
         if closeEvent is not None:
